010fbc5b6eae39b4a122600e58e7b703ddb0a3bd
Stanislav Sazonov <slavk11@yandex.ru>
2023-10-30 21:08:22 +0300

    Added SnapKit to project

---
+++ Clima/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  Clima
+//
+//  Created by Сазонов Станислав on 30.10.2023.
+//
+
+import Foundation
+
+enum Constants {
+    static let background = "background"
+    static let weatherColor = "weatherColor"
+    static let celsius = "°C"
+    static let searchSF = "magnifyinglass"
+    static let geoSF = "location.circle.fill"
+    static let search = "Search"
+    static let coditionSF = "cloud.rain"
+}
--- Clima/ViewController.swift
+++ Clima/ViewController.swift
@@ -6,14 +6,45 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    private lazy var mainStackView: UIStackView = {
+        let element = UIStackView()
+        
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var backgroundImageView: UIImageView = {
+        let element = UIImageView()
+        element.image = UIImage(named: Constants.background)
+        element.contentMode = .scaleAspectFill
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        
+        setView()
+        setupConstraints()
+
     }
-
-
+    
+    private func setView() {
+        view.addSubview(backgroundImageView)
+    }
 }
 
+extension ViewController {
+    
+    private func setupConstraints() {
+        backgroundImageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    
+}
